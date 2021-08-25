148be136be0f2817a0d87c5df8a8fb3d0a055885
Peter Edwards <sweetlilmre@gmail.com>
2021-08-25 11:23:39 +0200

    NOTE: enoder pins have CHANGED, please update your builds accordingly

--- include/config.h
+++ include/config.h
@@ -1,19 +1,26 @@
 #pragma once
-
 #define ROTARY_ENCODER_A_PIN 25
-#define ROTARY_ENCODER_B_PIN 33
-#define ROTARY_ENCODER_BUTTON_PIN 32
+#define ROTARY_ENCODER_B_PIN 32
+#define ROTARY_ENCODER_BUTTON_PIN 33
 #define C64_SENSE_PIN 16
 #define C64_MOTOR_PIN 17
 #define C64_READ_PIN 5
 #define C64_WRITE_PIN 18
 
-#define I2C_DISP_ADDR 0x3C
 #define I2C_DISP_ROWS 2
 #define I2C_DISP_COLS 16
 
-#define I2C_DISP_HD44780 0
-#define I2C_DISP_SSD1306 1
-#define I2C_DISP_TYPE I2C_DISP_SSD1306
-//I2C_DISP_HD44780
+#define I2C_DISP_TYPE_HD44780 0
+#define I2C_DISP_TYPE_SSD1306 1
+
 #define LCD_SSD1306_128x64
+
+#include "config-user.h"
+
+#ifndef I2C_DISP_ADDR
+#error Please define "I2C_DISP_ADDR" in config-user.h. Create this file if it does not exist!
+#endif
+
+#ifndef I2C_DISP_TYPE
+#error Please define "I2C_DISP_TYPE" as "I2C_DISP_TYPE_HD44780" or "I2C_DISP_TYPE_SSD1306" in config-user.h. Create this file if it does not exist!
+#endif
