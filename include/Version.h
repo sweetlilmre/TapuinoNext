70e8fad680745585f1fb7efe3627786aa7507ef9
Peter Edwards <sweetlilmre@gmail.com>
2022-03-17 15:02:28 +0200

    version bump again

--- include/Version.h
+++ include/Version.h
@@ -1,10 +1,10 @@
 
 #ifndef FW_BUILD_NUMBER
-  #define FW_BUILD_NUMBER "10"
+  #define FW_BUILD_NUMBER "11"
 #endif
 #ifndef FW_VERSION
-  #define FW_VERSION "0.0.1-alpha+10"
+  #define FW_VERSION "0.0.1-alpha+11"
 #endif
 #ifndef FW_BUILD_TIME
-  #define FW_BUILD_TIME "2022-03-17 14:59:21.139620"
+  #define FW_BUILD_TIME "2022-03-17 15:02:14.184889"
 #endif
