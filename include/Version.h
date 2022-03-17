f6b3fb1a38088362804f81255f1ed83f50ffbf2f
Peter Edwards <sweetlilmre@gmail.com>
2022-03-17 15:28:20 +0200

    fixing version in release action

--- include/Version.h
+++ include/Version.h
@@ -1,10 +1,10 @@
 
 #ifndef FW_BUILD_NUMBER
-  #define FW_BUILD_NUMBER "12"
+  #define FW_BUILD_NUMBER "13"
 #endif
 #ifndef FW_VERSION
-  #define FW_VERSION "0.0.1-alpha+12"
+  #define FW_VERSION "0.0.1-alpha+13"
 #endif
 #ifndef FW_BUILD_TIME
-  #define FW_BUILD_TIME "2022-03-17 15:25:01.143189"
+  #define FW_BUILD_TIME "2022-03-17 15:28:06.130329"
 #endif
