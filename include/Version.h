9939f75e1f9b9058e878a5dd7968945adac517b9
Peter Edwards <sweetlilmre@gmail.com>
2022-06-04 14:38:25 +0200

    testing release notes

--- include/Version.h
+++ include/Version.h
@@ -1,11 +1,11 @@
 
     #ifndef FW_BUILD_NUMBER
-      #define FW_BUILD_NUMBER "237"
+      #define FW_BUILD_NUMBER "238"
     #endif
     #ifndef FW_VERSION
-      #define FW_VERSION "0.0.2-alpha+237"
+      #define FW_VERSION "0.0.2-alpha+238"
     #endif
     #ifndef FW_BUILD_TIME
-      #define FW_BUILD_TIME "2022-06-04 12:54:40.389380"
+      #define FW_BUILD_TIME "2022-06-04 14:37:08.129687"
     #endif
     
