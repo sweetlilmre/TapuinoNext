669ae488058667896752bf988ea299be236aae91
Peter Edwards <sweetlilmre@gmail.com>
2022-12-06 11:04:11 +0200

    Fixed action version problem

--- include/Version.h
+++ include/Version.h
@@ -1,11 +1,11 @@
 
     #ifndef FW_BUILD_NUMBER
-      #define FW_BUILD_NUMBER "252"
+      #define FW_BUILD_NUMBER "253"
     #endif
     #ifndef FW_VERSION
-      #define FW_VERSION "v0.0.3-alpha+252"
+      #define FW_VERSION "v0.0.3-alpha+253"
     #endif
     #ifndef FW_BUILD_TIME
-      #define FW_BUILD_TIME "2022-12-06 09:42:38.215324"
+      #define FW_BUILD_TIME "2022-12-06 11:03:41.173299"
     #endif
     
