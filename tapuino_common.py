2ff1bbda433b75b211f6067d946f618ffb20cdf7
Peter Edwards <sweetlilmre@gmail.com>
2022-12-06 12:20:08 +0200

    more packing testing

--- tapuino_common.py
+++ tapuino_common.py
@@ -27,13 +27,3 @@ def pretty_exit_error(message):
 def pretty_exit_ok(message):
     print(message)
     exit(0)
-
-def fetch_packer_script():
-    file_url = "https://raw.githubusercontent.com/letscontrolit/ESPEasy/3678488f6a67ac9b899fab3d0ccd176c4437b1b4/tools/pio/post_esp32.py"
-    response = requests.get(file_url)
-    if response.status_code == 200:
-        with open("post_esp32.py", "wb") as f:
-            f.write(response.content)
-        print("File downloaded successfully.")
-    else:
-        print("Failed to download file. Status code:", response.status_code)
