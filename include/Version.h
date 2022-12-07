8676ebb9876685ef90fa167092f4809dc12088ed
Peter Edwards <sweetlilmre@gmail.com>
2022-12-07 22:45:13 +0200

    test

--- include/Version.h
+++ include/Version.h
@@ -1,11 +1,11 @@
 
     #ifndef FW_BUILD_NUMBER
-      #define FW_BUILD_NUMBER "263"
+      #define FW_BUILD_NUMBER "264"
     #endif
     #ifndef FW_VERSION
-      #define FW_VERSION "v0.0.3-alpha+263"
+      #define FW_VERSION "v0.0.3-alpha+264"
     #endif
     #ifndef FW_BUILD_TIME
-      #define FW_BUILD_TIME "2022-12-07 22:32:21.589312"
+      #define FW_BUILD_TIME "2022-12-07 22:44:56.933021"
     #endif
     
