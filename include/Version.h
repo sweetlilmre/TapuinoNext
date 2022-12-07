3ea6e74d3145e4b50c3d59b25bcfdfe293b7c278
Peter Edwards <sweetlilmre@gmail.com>
2022-12-07 22:40:39 +0200

    Added factory image script back, updated attibution to reflect origin

--- include/Version.h
+++ include/Version.h
@@ -1,11 +1,11 @@
 
     #ifndef FW_BUILD_NUMBER
-      #define FW_BUILD_NUMBER "256"
+      #define FW_BUILD_NUMBER "263"
     #endif
     #ifndef FW_VERSION
-      #define FW_VERSION "v0.0.3-alpha+256"
+      #define FW_VERSION "v0.0.3-alpha+263"
     #endif
     #ifndef FW_BUILD_TIME
-      #define FW_BUILD_TIME "2022-12-06 12:19:25.172191"
+      #define FW_BUILD_TIME "2022-12-07 22:32:21.589312"
     #endif
     
